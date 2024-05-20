8d8865ee73e8cdb5580e87009e5cf9ef89ddbcd1
YefersonMauricioDiazChogo21 <yefersondiaz2001@gmail.com>
2024-05-20 13:07:42 -0500

    login start

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import { UsersApp } from './src/users/users-app.js'
+import { addLogin } from './src/users/login.js'
 
 const boton = document.querySelector('.activar')
-
+addLogin()
 boton.addEventListener('click', () => {
     document.querySelector('#app').innerHTML = `
     
