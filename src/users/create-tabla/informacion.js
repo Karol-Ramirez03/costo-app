82a148f46e9fd5cb605086f58c1b26a12cb00054
Lauraw100 <lauvanetorres1223@gmail.com>
2024-05-24 18:47:31 -0500

    end 3

--- src/users/create-tabla/informacion.js
+++ src/users/create-tabla/informacion.js
@@ -1,187 +1,242 @@
 export const info = [
-    {
-        about: "¿Que nivel de calidad estas buscando?",
-        name: "Calidad",
-        imagenes: [
-            {
-                src: "./src/users/create-tabla/imagenes/P-1/calidadOptima.png",
-                value: "",
-                descripcion: "Calidad Optima",
-            },
-            {
-                src: "./src/users/create-tabla/imagenes/P-1/calidadPrecio.png",
-                value: "",
-                descripcion: "Buena Relacion Calidad/Precio",
-            },
-            {
-                src: "./src/users/create-tabla/imagenes/P-1/noImportaLaCalidad.png",
-                value: "",
-                descripcion: "No me importa tanto la calidad",
-            },
-        ]
-    },
-    {
-        about: "¿Qué tipo de app necesitas?",
-        name: "tipo de app",
-        imagenes: [
-            {
-                src: "src/users/create-tabla/imagenes/P-2/android.png",
-                value: "3",
-                descripcion: "Aplicacion Android",
-            },
-            {
-                src: "src/users/create-tabla/imagenes/P-2/ios.png",
-                value: "",
-                descripcion:"Aplicacion iOS",
-            },
-            {
-                src: "src/users/create-tabla/imagenes/P-2/windows-phone.png",
-                value: "",
-                descripcion: "Aplicacion Windows Phone",
-            },
-            {
-                src: "src/users/create-tabla/imagenes/P-2/android-ios.png",
-                value: "",
-                descripcion: "Aplicacion Android + iOS",
-            },
-            {
-                src: "",
-                value: "",
-                descripcion: "Aplicacion De Escritorio",
-            },
-        ]
-    },
-    {
-        about: "¿Qué diseño quieres que tenga tu App?",
-        name: "diseño",
-        imagenes: [
-            {
-                src: "src/users/create-tabla/imagenes/P-3/sencilla.png",
-                value: "",
-                descripcion:"Interfaz sencilla",
-            },
-            {
-                src: "src/users/create-tabla/imagenes/P-3/personalizada.png",
-                value: "",
-                descripcion:"Interfaz personalizada",
-            },
-            {
-                src: "src/users/create-tabla/imagenes/P-3/web.png",
-                value: "",
-                descripcion: "Interfaz Replicada De la Web",
-            },
-            {
-                src: "src/users/create-tabla/imagenes/P-3/sinDiseño.png",
-                value: "",
-                descripcion: "No Necesito diseño",
-            },
-        ]
-    },
-    {
-        about: "¿Cómo quieres sacar beneficio a tu App?",
-        name: "beneficios app",
-        imagenes: [
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-            {
-                src: "src/users/create-tabla/imagenes/P-4/comprasEnApp.png",
-                value: "",
-                descripcion: "Compras Dentro de la app ",
-            },
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-        ]
-    },
-    {
-        about: "",
-        name: "",
-        imagenes: [
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-        ]
-    },
-    {
-        about: "",
-        name: "",
-        imagenes: [
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-        ]
-    },
-    {
-        about: "",
-        name: "",
-        imagenes: [
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-        ]
-    },
-    {
-        about: "",
-        name: "",
-        imagenes: [
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-        ]
-    },
-    {
-        about: "",
-        name: "",
-        imagenes: [
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-            {
-                src: "",
-                value: "",
-                descripcion: "",
-            },
-        ]
-    },
-    
+  {
+    about: "¿Que nivel de calidad estas buscando?",
+    name: "Calidad",
+    imagenes: [
+      {
+        src: "./src/users/create-tabla/imagenes/P-1/calidadOptima.png",
+        value: "2000000",
+        descripcion: "Calidad Optima",
+      },
+      {
+        src: "./src/users/create-tabla/imagenes/P-1/calidadPrecio.png",
+        value: "4000000",
+        descripcion: "Buena Relacion Calidad/Precio",
+      },
+      {
+        src: "./src/users/create-tabla/imagenes/P-1/noImportaLaCalidad.png",
+        value: "1000000",
+        descripcion: "No me importa tanto la calidad",
+      },
+    ],
+  },
+  {
+    about: "¿Qué tipo de app necesitas?",
+    name: "tipo",
+    imagenes: [
+      {
+        src: "src/users/create-tabla/imagenes/P-2/android.png",
+        value: "9000000",
+        descripcion: "Aplicacion Android",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-2/ios.png",
+        value: "15000000",
+        descripcion: "Aplicacion iOS",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-2/windows-phone.png",
+        value: "9000000",
+        descripcion: "Aplicacion Windows Phone",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-2/android-ios.png",
+        value: "9000000",
+        descripcion: "Aplicacion Android + iOS",
+      },
+      {
+        src: "",
+        value: "9000000",
+        descripcion: "Aplicacion De Escritorio",
+      },
+    ],
+  },
+  {
+    about: "¿Qué diseño quieres que tenga tu App?",
+    name: "diseno",
+    imagenes: [
+      {
+        src: "src/users/create-tabla/imagenes/P-3/sencilla.png",
+        value: "6000000",
+        descripcion: "Interfaz sencilla",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-3/personalizada.png",
+        value: "18000000",
+        descripcion: "Interfaz personalizada",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-3/web.png",
+        value: "12000000",
+        descripcion: "Interfaz Replicada De la Web",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-3/sinDiseño.png",
+        value: "0",
+        descripcion: "No Necesito diseño",
+      },
+    ],
+  },
+  {
+    about: "¿Cómo quieres sacar beneficio a tu App?",
+    name: "beneficios app",
+    imagenes: [
+      {
+        src: "src/users/create-tabla/imagenes/P-4/free.png",
+        value: "750000",
+        descripcion: "Aplicación gratuita con publicidad",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-4/pago.png",
+        value: "750000",
+        descripcion: "Aplicación de pago",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-4/comprasEnApp.png",
+        value: "6000000",
+        descripcion: "Compras Dentro de la app ",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-4/otros.png",
+        value: "1500000",
+        descripcion: "Otros / No lo sé todavía",
+      },
+    ],
+  },
+  {
+    about: "¿Tu App necesita un sistema de login ?",
+    name: "Sistema Login",
+    imagenes: [
+      {
+        src: "src/users/create-tabla/imagenes/P-5/redes.png",
+        value: "6000000",
+        descripcion: "Sí, con redes sociales y email",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-5/email.png",
+        value: "3750000",
+        descripcion: "Sí, con email",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-5/no.png",
+        value: "0",
+        descripcion: "No",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-5/noSe.png",
+        value: "3000000",
+        descripcion: "No lo sé todavía",
+      },
+    ],
+  },
+  {
+    about: "¿Tu App tiene que estar integrada con un sitio web?",
+    name: "Integrada",
+    imagenes: [
+      {
+        src: "src/users/create-tabla/imagenes/P-6/si.png",
+        value: "6000000",
+        descripcion: "si",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-6/no.png",
+        value: "0",
+        descripcion: "No",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-6/noSe.png",
+        value: "3000000",
+        descripcion: "No lo se",
+      },
+    ],
+  },
+  {
+    about: "¿Los usuarios tienen sus propios perfiles?",
+    name: "perfiles propios ",
+    imagenes: [
+      {
+        src: "src/users/create-tabla/imagenes/P-7/si.png",
+        value: "6000000",
+        descripcion: "Si",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-7/no.png",
+        value: "0",
+        descripcion: "No",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-7/noSe.png",
+        value: "3000000",
+        descripcion: "No lo se",
+      },
+    ],
+  },
+  {
+    about: "¿Tu app necesita un panel de administración?",
+    name: "Panel Adminimstracion",
+    imagenes: [
+      {
+        src: "src/users/create-tabla/imagenes/P-8/si.png",
+        value: "6000000",
+        descripcion: "Si",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-8/No.png",
+        value: "0",
+        descripcion: "No",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-8/noSe.png",
+        value: "3000000",
+        descripcion: "No se",
+      },
+    ],
+  },
+  {
+    about: "¿Qué idiomas usará tu aplicación?",
+    name: "Idiomas",
+    imagenes: [
+      {
+        src: "src/users/create-tabla/imagenes/P-9/unidioma.png",
+        value: "500000",
+        descripcion: "Un único idioma",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-9/bilingue.png",
+        value: "1000000",
+        descripcion: "Bilingüe",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-9/multilingue.png",
+        value: "2000000",
+        descripcion: "Multilingüe",
+      },
+    ],
+  },
+  {
+    about: "¿En qué estado se encuentra tu proyecto?",
+    name: "Idiomas",
+    imagenes: [
+      {
+        src: "src/users/create-tabla/imagenes/P-10/Idea.png",
+        value: "4000000",
+        descripcion: "Sólo es una idea",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-10/boceto.png",
+        value: "2000000",
+        descripcion: "Boceto ya preparado",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-10/Desarrollo.png",
+        value: "1000000",
+        descripcion: "App en desarrollo",
+      },
+      {
+        src: "src/users/create-tabla/imagenes/P-10/Hecha.png",
+        value: "0",
+        descripcion: "App ya desarrollada",
+      },
+    ],
+  },
 ];
